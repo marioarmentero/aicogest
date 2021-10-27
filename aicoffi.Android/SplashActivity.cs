8c416806829b71fe1f84492e427abe3f2136022d
marmentero <marmentero@seipsystems.com>
2021-10-27 22:26:41 +0200

    Agregar archivos de proyecto.

---
+++ aicoffi.Android/SplashActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace aicoffi.Droid
+{
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            System.Threading.Thread.Sleep(600);
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+            // Create your application here
+        }
+    }
+}
